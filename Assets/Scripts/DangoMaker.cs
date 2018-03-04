19c203c1ff139377342b16deb675d4d8b66c7381
Julia Harding <puppywayne@gmail.com>
2018-03-04 12:28:53 -1000

    Octopai Rate Increase
    dieeee

--- Assets/Scripts/DangoMaker.cs
+++ Assets/Scripts/DangoMaker.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DangoMaker : MonoBehaviour {
-	public GameObject[] dangos;
+	public List<GameObject> dangos;
 	public float dangoRate = 1;
 	private float timeSinceLastDango = 0;
 	private HandController hand;
+	public GameObject octopus;
+	private int numberOfDangos;
+	private Player player;
 
 	// Use this for initialization
 	void Start () {
 		hand = GameObject.Find ("Hand").GetComponent<HandController> ();
+		player = GameObject.Find ("Player").GetComponent<Player> ();
+		numberOfDangos = dangos.Count;
+
 	}
 
 	void FixedUpdate () {
@@ -20,12 +26,18 @@ public class DangoMaker : MonoBehaviour {
 		} else {
 			timeSinceLastDango = timeSinceLastDango + Time.fixedDeltaTime;
 		}
+		var score = player.getScore ();
+		var numberOfOctopai = dangos.Count - numberOfDangos;
+		var expectedOctopai = score / 20 + 1;
+		if (expectedOctopai > numberOfOctopai) {
+			dangos.Add (octopus);
+		}
 	}
 
 	void CreateDango () {
 		float xCoordinate = (Random.value * 30.0f) - 15.0f;
 		float yCoordinate = 32.0f + hand.getHeight();
-		int dangoIndex = Random.Range (0, dangos.Length);
+		int dangoIndex = Random.Range (0, dangos.Count);
 		GameObject dango = (GameObject)Instantiate (dangos [dangoIndex]);
 		dango.transform.position = new Vector3 (xCoordinate, yCoordinate, -1);
 	}
