9ef32375a00162a4b3ecfe44746dae1f59058c29
Julia Harding <puppywayne@gmail.com>
2018-10-07 12:06:09 -1000

    Reload Play Game is Delayed

---
+++ Assets/Scripts/DeadSceneManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeadSceneManager : MonoBehaviour {
+	public Button playAgainButton;
+
+	// Use this for initialization
+	void Start () {
+		playAgainButton.interactable = false;
+		StartCoroutine (LoadLevelAfterDelay (1f));
+	}
+	
+	IEnumerator LoadLevelAfterDelay(float delay)
+	{
+		yield return new WaitForSeconds(delay);
+		print ("Delayed!");
+		playAgainButton.interactable = true;
+	}
+}
